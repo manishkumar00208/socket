2e739c8af0e6ded715232a3373591a5d3a11a649
Manish kumar chintha <chintamanishkumar@gmail.com>
2022-01-31 18:16:02 +0530

    source code

--- client.py
+++ client.py
@@ -1,1 +1,12 @@
+import socket
+
+c = socket.socket()
+
+# enter server's ip address instead localhost when you run this on other machine
+c.connect(('localhost',16345))
+
+name = input("enter ur name")
+c.send(bytes(name,'utf-8'))
+
+print(c.recv(1024).decode())
 
--- server.py
+++ server.py
@@ -1,1 +1,24 @@
 
+import socket
+
+# by default IPv4 and TCP
+s = socket.socket()
+
+print("socket created")
+
+# binding a socket with a port number
+s.bind(('localhost', 16345))
+
+# no.of connections
+s.listen(3)
+print('waiting for connections')
+
+while True:
+    c, address = s.accept()
+
+    name = c.recv(1024).decode()
+    print("connected with ", address, name)
+
+    c.send(bytes('welcome client','utf-8'))
+
+    c.close()
